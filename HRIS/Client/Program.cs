88fd12986bb53fcdb134bde7f9fb7ca147e2343f
JM Mabasa <JM Mabasa@DESKTOP-6LTROHR>
2023-10-05 17:15:01 +0800

    Add Dialog for Edit button

--- HRIS/Client/Program.cs
+++ HRIS/Client/Program.cs
@@ -14,6 +14,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddMudBlazorDialog();
+
 
 //INTERFACES
 builder.Services.AddScoped<IDivisionService, DivisionService>();
